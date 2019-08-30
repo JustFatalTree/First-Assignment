95e27a99278cbd5c01b8d2e48dbf7a7c28fe9090
Pierce Hardy <pierce.hardy@tooeleschools.org>
2019-08-30 13:11:11 -0600

    Finished AssignmentOne

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -1,9 +1,9 @@
-
+import java.util.Calendar;
 public class FirstAssignment{
 
 
 
 	public static void main (String[] args) {
-		System.out.println("First Assignmet") ;
+		System.out.println(Calendar.getInstance().getTime());
 		}
 }		
